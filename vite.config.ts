4ebdde738de1e313349d07d2d047c8dc9a566bd2
crimsonpinus <crimsonpinus@outlook.com>
2025-06-18 23:58:53 +0900

    Allow External access

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import  tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: true, // 외부 접속 허용
+    port: 5173, // 필요시 포트 변경
+    strictPort: true,
+    watch: { usePolling: true }
+  }
 })
